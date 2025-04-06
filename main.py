c119bb439f2e2bc094bd48636c158c0619abe528
dexmcmillan <dexmcmillan@gmail.com>
2025-04-06 10:18:45 -0400

    change to track only the table contents

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
+import bs4
 
 r = requests.get('https://www.canada.ca/en/department-finance/news/2025/03/list-of-products-from-the-united-states-subject-to-25-per-cent-tariffs-effective-march-13-2025.html')
+
+soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
+# Find the table with tariffs
+table = soup.find('table')
+
+print(table)
 
 with open('tariffs.html', 'w') as f:
     f.write(r.text)
